5b1112fc54f9b97de088cbecd071bef582e9540b
Krieo <krieolin@gmail.com>
2023-08-15 15:58:13 +0200

    updated the way the scanner works

--- Program.cs
+++ Program.cs
@@ -34,6 +34,29 @@ class Scanner
         ("DO", new Regex(@"\bDO\b")),
         ("IF", new Regex(@"\bIF\b")),
         ("PLUS", new Regex(@"\+")),
+      ("MINUS", new Regex(@"\-")),
+    ("FORWARD SLASH", new Regex(@"/")),
+    ("STAR", new Regex(@"\*")),
+    ("ASSIGN", new Regex(@"=")),
+    ("EQUAL", new Regex(@"==")),
+    ("NEQ", new Regex(@"<>")),
+    ("GREATEREQUAL", new Regex(@">=")),
+    ("LESSEREQUAL", new Regex(@"<=")),
+    ("GREATER", new Regex(@">")),
+    ("LESSER", new Regex(@"<")),
+    ("SEMICOLON", new Regex(@";")),
+    ("LEFTPARENTHESIS", new Regex(@"\(")),
+    ("RIGHTPARENTHESIS", new Regex(@"\)")),
+    ("STRING", new Regex(@"\bSTRING\b")),
+    ("BOOL", new Regex(@"\bBOOL\b")),
+  ("STRINGLIT", new Regex(@"""(?:[a-zA-Z0-9~!@#$%^&*`()\[\]_\-+=|{};:<>,.?\\])*")),
+
+    ("INTEGERLIT", new Regex(@"[0-9]+")),
+    ("FLOATLIT", new Regex(@"[0-9]*\.[0-9]*")),
+    ("ID", new Regex(@"([a-zA-Z]|[0-9])([a-zA-Z]|[0-9])*")),
+    ("BOOLLITTRUE", new Regex(@"\bTRUE\b")),
+    ("BOOLLITFALSE", new Regex(@"\bFALSE\b")),
+    ("ARRAY", new Regex(@"\bARRAY\b")),
 
 };
 
@@ -70,9 +93,55 @@ class Scanner
 
 class Program
 {
+    static string ReadTextFromFile(string filePath)
+    {
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            return content;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+            return null;
+        }
+    }
+
     static void Main(string[] args)
     {
-        string input = "RECIPE HelloWorld DO METHOD Main() AS INTEGER DO SPEAK(\"Hello, World!\"); DONE DONE";
+        // string input = "RECIPE HelloWorld DO METHOD Main() AS INTEGER DO SPEAK(\"Hello, World!\"); DONE DONE";
+        /*
+          string input = @"RECIPE MathOperations
+                      DO
+                      METHOD Main() AS INTEGER
+                      DO
+                      INGREDIENT a AS INTEGER;
+                      INGREDIENT b AS INTEGER;
+                      a = 14;
+                      b = 3;
+                      INGREDIENT sum AS INTEGER;
+                      sum = a + b;
+                      INGREDIENT product AS INTEGER;
+                      product = a * b;
+                      INGREDIENT quotient AS FLOAT;
+                      quotient = a / b;
+                      SPEAK(""Sum: "" + sum);
+                      SPEAK(""Product: "" + product);
+                      SPEAK(""Quotient: "" + quotient);
+                      SHARE 0;
+                      DONE
+                      DONE";
+         */
+        string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        string dataFolderPath = Path.Combine(projectDirectory, "sample programs");
+        string filename = "sample1.txt"; 
+
+        string filePath = Path.Combine(dataFolderPath, filename);
+        Console.WriteLine(filePath);
+        Console.ReadLine();
+        string input = ReadTextFromFile(filePath);
+         
+        input = ReadTextFromFile(filePath);
 
         Scanner scanner = new Scanner();
         List<Token> tokens = scanner.Tokenize(input);
