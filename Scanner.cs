b67465657ded9f1325000e5f512b9055a07268fe
Krieo <krieolin@gmail.com>
2023-09-08 14:47:22 +0200

    updated scanner to work with longest match

--- Scanner.cs
+++ Scanner.cs
@@ -4,15 +4,8 @@ using System.Text.RegularExpressions;
 
 namespace _219003234_Naidoo_KN_CC
 {
-    /// <summary>
-    /// This class is the scanner for the language
-    /// </summary>
     public class Scanner
     {
-        // This list represents all of the tokens used in the language (type and lexemes)
-        // The items in the list will get matched in order, which is why keywords are ordered from highest priority to lowest.
-        //although i also added \b which acts as word boundaries so that a word will not get matched if it is a substring of 
-        //another word so if i input "INTEGERHELLO" this will not match to integer but instead ID
         private readonly List<(string TokenType, Regex Pattern)> tokenDefinitions = new List<(string, Regex)>
         {
             ("RECIPE", new Regex(@"\bRECIPE\b")),
@@ -46,20 +39,18 @@ namespace _219003234_Naidoo_KN_CC
             ("RIGHTPARENTHESIS", new Regex(@"\)")),
             ("STRING", new Regex(@"\bSTRING\b")),
             ("BOOL", new Regex(@"\bBOOL\b")),
-            ("STRINGLIT", new Regex(@"""(?:[a-zA-Z0-9~!@#$%^&*`()\[\]_\-+=|{};:<>,.?\\])*")),
+            ("STRINGLIT", new Regex(@"""(?:[^""\\]*(?:\\.[^""\\]*)*)""")),
             ("INTEGERLIT", new Regex(@"[0-9]+")),
-            ("FLOATLIT", new Regex(@"[0-9]*\.[0-9]*")),
+            ("FLOATLIT", new Regex(@"[0-9]*\.[0-9]+")),
             ("BOOLLITTRUE", new Regex(@"\bTRUE\b")),
             ("BOOLLITFALSE", new Regex(@"\bFALSE\b")),
             ("ARRAY", new Regex(@"\bARRAY\b")),
-            ("ID", new Regex(@"\b([a-zA-Z]|[0-9])([a-zA-Z]|[0-9])*\b")),
+            ("ID", new Regex(@"[a-zA-Z][a-zA-Z0-9]*")),
+            ("WHITESPACE", new Regex(@"\s+")),
+            ("COMMENT", new Regex(@"//.*?(?=\r|\n|$)", RegexOptions.Singleline)),
+            ("ERROR", new Regex(@"\S")),
         };
 
-        /// <summary>
-        /// This method tokenizes the input and tries to match it to some tokens in the list above 
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
         public List<Token> Tokenize(string input)
         {
             List<Token> tokens = new List<Token>();
@@ -67,25 +58,30 @@ namespace _219003234_Naidoo_KN_CC
 
             while (currentPosition < input.Length)
             {
-                bool matched = false;
+                int maxLength = 0;
+                Token? longestMatch = null;
 
                 foreach ((string tokenType, Regex pattern) in tokenDefinitions)
                 {
                     Match match = pattern.Match(input, currentPosition);
 
-                    if (match.Success && match.Index == currentPosition)
+                    if (match.Success && match.Index == currentPosition && match.Length > maxLength)
                     {
-                        matched = true;
-                        tokens.Add(new Token(tokenType, match.Value));
-                        currentPosition += match.Length;
-                        break;
+                        maxLength = match.Length;
+                        longestMatch = new Token(tokenType, match.Value);
                     }
                 }
 
-                if (!matched)
+                if (longestMatch != null)
                 {
-                    // Skip unrecognized characters
-                    currentPosition++;
+                    tokens.Add(longestMatch);
+                    currentPosition += maxLength;
+                }
+                else
+                {
+                    // Unrecognized character
+                    Console.WriteLine($"Unrecognized character at position {currentPosition}: '{input[currentPosition]}'");
+                    currentPosition++; // Move to the next character
                 }
             }
 
@@ -93,15 +89,3 @@ namespace _219003234_Naidoo_KN_CC
         }
     }
 }
-
-
-
-
-/*
- * 
- so just something cool about how the types matches i used word boundaries in the regular expression represented by "\b"
-how it essentially works is that it says a word must match an entire word and not be apart of another word so for example
-if an input is "INTEGERHELLO" normally this would match the type to an INTEGER and not an ID but because it is part
-of another word it doesn't match it to INTEGER and instead matches it to ID
- 
- */
