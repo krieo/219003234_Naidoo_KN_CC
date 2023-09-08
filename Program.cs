01ed4f9f92535f7b849370a63b6275694dd91400
Krieo <krieolin@gmail.com>
2023-09-08 14:47:35 +0200

    updated program to remove trace of parser

--- Program.cs
+++ Program.cs
@@ -1,18 +1,9 @@
-﻿using _219003234_Naidoo_KN_CC;
-using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
+using _219003234_Naidoo_KN_CC;
 
-/// <summary>
-/// This is the main program class that acts as a starting point for the program to run
-/// </summary>
 class Program
 {
-    /// <summary>
-    /// This method is used to read from a file
-    /// </summary>
-    /// <param name="filePath"></param>
-    /// <returns></returns>
     static string ReadTextFromFile(string filePath)
     {
         try
@@ -27,21 +18,13 @@ class Program
         }
     }
 
-    /// <summary>
-    /// The main method that is the starting point
-    /// This method loads a sample file from 1 to 8 from the sample programs folder
-    /// each file contains one of the example sample programs that is included in the documentation
-    /// it then performs the scanner operations by trying to match input to tokens with there underlying type and lexeme
-    /// </summary>
-    /// <param name="args"></param>
     static void Main(string[] args)
     {
- 
         string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
         string dataFolderPath = Path.Combine(projectDirectory, "sample programs");
         string filename = "sample8.txt";
 
-        Console.WriteLine("Enter number to load file from 1 to 8 (anyother input will result in file 8 being loaded):");
+        Console.WriteLine("Enter number to load file from 1 to 8 (any other input will result in file 8 being loaded):");
         string fileNumber = Console.ReadLine();
 
         switch (fileNumber)
@@ -75,24 +58,20 @@ class Program
                 break;
         }
 
-
         string filePath = Path.Combine(dataFolderPath, filename);
         Console.WriteLine(filePath);
-     
+
         string input = ReadTextFromFile(filePath);
-         
-        input = ReadTextFromFile(filePath);
 
-
-        //This prints the contents from the file to the screen
+        // Print the contents from the file to the screen
         Console.WriteLine(input);
         Console.WriteLine("\n");
-     
-        
-        //This prints out the type and lexeme pairs from the input class
+
+        // Create an instance of the Scanner class and tokenize the input
         Scanner scanner = new Scanner();
         List<Token> tokens = scanner.Tokenize(input);
 
+        // Print out the type and lexeme pairs from the tokenized input
         foreach (Token token in tokens)
         {
             Console.WriteLine($"Type: {token.Type}, Lexeme: {token.Lexeme}");
