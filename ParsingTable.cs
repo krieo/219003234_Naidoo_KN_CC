97e8629570d8d86bd17a498bf724c8bd17a4cb87
Krieo <krieolin@gmail.com>
2023-09-20 11:20:11 +0200

    updated parser and production rules

--- ParsingTable.cs
+++ ParsingTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 public class ParsingTable
 {
@@ -8,59 +9,90 @@ public class ParsingTable
     public ParsingTable()
     {
         productionRules = new Dictionary<string, List<List<string>>>();
-        // Initialize the production rules here
-        AddProduction("RECIPE", new List<List<string>> { new List<string> { "RECIPE", "ID", "DO", "METHOD_MAIN", "DONE" } });
-        AddProduction("METHOD_MAIN", new List<List<string>> { new List<string> { "METHOD", "MAIN", "LEFTPARENTHESIS", "RIGHTPARENTHESIS", "DO", "STMNT_BLOCK", "DONE" } }); //YOU NEED TO UPDATE THE PRODUCTION RULES SO IT MATCHES CHECK WHATSAPP FOR HOW YOU WERE GOING TO DO IT/ THESE ARE ALSO WRONG IT NEEDS AS INTEGER/STRING/BOOLEAN ETC
-        AddProduction("STMNT_BLOCK", new List<List<string>> { new List<string> { "STMNT", "STMNT_BLOCK" }, new List<string> { } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "ASK", "LEFTPARENTHESIS", "ID", "RIGHTPARENTHESIS", "SEMICOLON" } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "SPEAK", "LEFTPARENTHESIS", "STRINGLIT", "RIGHTPARENTHESIS", "SEMICOLON" } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "SHARE", "INTEGERLIT", "SEMICOLON" } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "ID", "ASSIGN", "EXPR", "SEMICOLON" } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "ID", "ASSIGN", "ARRAY_ACCESS", "SEMICOLON" } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "IF", "EXPR", "THEN", "STMNT_BLOCK", "ELSE_BLOCK", "DONE" } });
-        AddProduction("ELSE_BLOCK", new List<List<string>> { new List<string> { "ELSE", "STMNT_BLOCK" } });
-        AddProduction("ELSE_BLOCK", new List<List<string>> { new List<string> { } });
-        AddProduction("STMNT", new List<List<string>> { new List<string> { "FUNCTION_CALL", "SEMICOLON" } });
-        AddProduction("EXPR", new List<List<string>> { new List<string> { "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "PLUS", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "MINUS", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "AND", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "GREATER", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "GREATEREQUAL", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "LESSER", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "LESSEREQUAL", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "EQUAL", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { "NEQ", "TERM", "EXPR_PRIME" } });
-        AddProduction("EXPR_PRIME", new List<List<string>> { new List<string> { } });
-        AddProduction("TERM", new List<List<string>> { new List<string> { "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "STAR", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "FORWARD_SLASH", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "AND", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "GREATER", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "GREATEREQUAL", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "LESSER", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "LESSEREQUAL", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "EQUAL", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { "NEQ", "FACTOR", "TERM_PRIME" } });
-        AddProduction("TERM_PRIME", new List<List<string>> { new List<string> { } });
-        AddProduction("FACTOR", new List<List<string>> { new List<string> { "ID" } });
-        AddProduction("FACTOR", new List<List<string>> { new List<string> { "INTEGERLIT" } });
-        AddProduction("FACTOR", new List<List<string>> { new List<string> { "STRINGLIT" } });
-        AddProduction("FACTOR", new List<List<string>> { new List<string> { "ARRAY_ACCESS" } });
-        AddProduction("FACTOR", new List<List<string>> { new List<string> { "FUNCTION_CALL" } });
-        AddProduction("ARRAY_ACCESS", new List<List<string>> { new List<string> { "ID", "LEFTBRACKET", "EXPR", "RIGHTBRACKET" } });
-        AddProduction("ARRAY_DECL", new List<List<string>> { new List<string> { "ID", "LEFTBRACKET", "ARRAY_SIZE", "RIGHTBRACKET", "SEMICOLON" } });
-        AddProduction("ARRAY_SIZE", new List<List<string>> { new List<string> { "INTEGERLIT" } });
-        AddProduction("ARRAY_SIZE", new List<List<string>> { new List<string> { "ID" } });
-        AddProduction("ARRAY_ASSIGNMENT", new List<List<string>> { new List<string> { "ID", "LEFTBRACKET", "EXPR", "RIGHTBRACKET", "ASSIGN", "EXPR", "SEMICOLON" } });
-        AddProduction("FUNCTION_CALL", new List<List<string>> { new List<string> { "AT_SIGN", "ID", "LEFTPARENTHESIS", "ARGUMENT_LIST", "RIGHTPARENTHESIS" } });
-        AddProduction("ARGUMENT_LIST", new List<List<string>> { new List<string> { "EXPR", "ARGUMENT_LIST_PRIME" } });
-        AddProduction("ARGUMENT_LIST_PRIME", new List<List<string>> { new List<string> { "COMMA", "EXPR", "ARGUMENT_LIST_PRIME" } });
-        AddProduction("ARGUMENT_LIST_PRIME", new List<List<string>> { new List<string> { } });
-        AddProduction("TYPE", new List<List<string>> { new List<string> { "INTEGER" } });
-        AddProduction("TYPE", new List<List<string>> { new List<string> { "FLOAT" } });
-        AddProduction("TYPE", new List<List<string>> { new List<string> { "STRING" } });
-        AddProduction("TYPE", new List<List<string>> { new List<string> { "BOOLEAN" } });
+
+        // Initialize the production rules for your language here
+        AddProduction("<program>", new List<List<string>>
+        {
+            new List<string> { "<recipe>" }
+        });
+
+        AddProduction("<recipe>", new List<List<string>>
+        {
+            new List<string> { "RECIPE", "<identifier>", "<block>" }
+        });
+        AddProduction("<block>", new List<List<string>>
+{
+    new List<string> { "DO", "<statement>", "DONE" }
+});
+
+
+        AddProduction("<identifier>", new List<List<string>>
+{
+    new List<string> { "ID" }
+});
+
+
+        // Add more production rules here
+        AddProduction("<statement>", new List<List<string>>
+{
+    new List<string> { "<variable-declaration>" },
+    new List<string> { "<assignment>" },
+    new List<string> { "<conditional-statement>" },
+    new List<string> { "<loop>" },
+    new List<string> { "<function-call>" },
+    new List<string> { "<input>" },
+    new List<string> { "<output>" },
+    new List<string> { "<return>" }
+});
+
+        AddProduction("<variable-declaration>", new List<List<string>>
+{
+    new List<string> { "INGREDIENT", "<identifier>", "AS", "<type>", ";" }
+});
+
+        AddProduction("<assignment>", new List<List<string>>
+{
+    new List<string> { "<identifier>", "=", "<expression>", ";" }
+});
+
+        AddProduction("<conditional-statement>", new List<List<string>>
+{
+    new List<string> { "IF", "<expression>", "THEN", "<block>", "ELSE", "<block>", "DONE" },
+    new List<string> { "IF", "<expression>", "THEN", "<block>", "DONE" }
+});
+
+        AddProduction("<loop>", new List<List<string>>
+{
+    new List<string> { "LOOP", "WHILE", "<expression>", "DO", "<block>", "DONE" }
+});
+
+        AddProduction("<function-call>", new List<List<string>>
+{
+    new List<string> { "@", "<identifier>", "(", "<expression-list>", ")", ";" }
+});
+
+        AddProduction("<input>", new List<List<string>>
+{
+    new List<string> { "ASK", "(", "<identifier>", ")", ";" }
+});
+
+        AddProduction("<output>", new List<List<string>>
+{
+    new List<string> { "SPEAK", "(", "<expression>", ")", ";" }
+});
+
+        AddProduction("<return>", new List<List<string>>
+{
+    new List<string> { "SHARE", "<expression>", ";" }
+});
+
+        AddProduction("<expression-list>", new List<List<string>>
+{
+    new List<string> { "<expression>", ",", "<expression-list>" },
+    new List<string> { "<expression>" }
+});
+
+
     }
 
     public void AddProduction(string nonTerminal, List<List<string>> production)
@@ -81,5 +113,21 @@ public class ParsingTable
         }
 
         throw new ArgumentException($"Production rule for {nonTerminal} not found.");
+    }
+
+    public string GenerateFormattedRules()
+    {
+        StringBuilder formattedRules = new StringBuilder();
+
+        foreach (var nonTerminal in productionRules.Keys)
+        {
+            formattedRules.AppendLine($"{nonTerminal} ::=");
+            foreach (var production in productionRules[nonTerminal])
+            {
+                formattedRules.AppendLine($"  {string.Join(" ", production)}");
+            }
+        }
+
+        return formattedRules.ToString();
     }
 }
--- SimpleParser.cs
+++ SimpleParser.cs
@@ -7,51 +7,78 @@ namespace _219003234_Naidoo_KN_CC
     public class SimpleParser
     {
         private List<string> tokenList;
-        private List<string> productionList;
+        private int currentPosition;
+        private ParsingTable parsingTable;
 
         public SimpleParser(List<string> tokens)
         {
             tokenList = tokens;
-            productionList = new List<string>();
+            currentPosition = 0; // Initialize the current position
+            parsingTable = new ParsingTable(); // Initialize the parsing table
         }
 
-        public bool Parse()
+        // Start the parsing process
+        public bool ParseProgram()
         {
-            while (tokenList.Count > 0 && productionList.Count > 0)
-            {
-                string currentToken = tokenList[0];
+            return ParseNonTerminal("<program>");
+        }
 
-                if (productionList[0] == currentToken)
+        // Parsing function for non-terminals
+        private bool ParseNonTerminal(string nonTerminal)
+        {
+            // Get the next token
+            string currentToken = currentPosition < tokenList.Count ? tokenList[currentPosition] : null;
+
+            // Get the production rules for the current non-terminal
+            List<List<string>> productionRules = parsingTable.GetProduction(nonTerminal);
+
+            // Log the current non-terminal and token
+            Console.WriteLine($"Parsing {nonTerminal} with current token: {currentToken}");
+
+            // Try each production rule
+            foreach (var production in productionRules)
+            {
+                int originalPosition = currentPosition; // Store the original position
+
+                // Log the current production
+                Console.WriteLine($"Trying production: {string.Join(" ", production)}");
+
+                // Try to match the production
+                bool productionMatched = true;
+                foreach (var symbol in production)
                 {
-                    // Current token in production list matches the current token in token list.
-                    tokenList.RemoveAt(0);
-                    productionList.RemoveAt(0);
-                    Console.WriteLine($"Matched token: {currentToken}");
+                    if (symbol.StartsWith("<"))
+                    {
+                        // Symbol is a non-terminal
+                        productionMatched = ParseNonTerminal(symbol);
+                    }
+                    else if (currentToken == symbol)
+                    {
+                        // Symbol is a terminal and matches the current token
+                        currentPosition++; // Consume the token
+                    }
+                    else
+                    {
+                        // Symbol is a terminal, but it doesn't match the current token
+                        productionMatched = false;
+                        break;
+                    }
+                }
+
+                if (productionMatched)
+                {
+                    Console.WriteLine("Production matched!");
+                    return true; // Successfully matched this production
                 }
                 else
                 {
-                    // No match, parsing failed.
-                    Console.WriteLine($"Parsing failed at token: {currentToken}");
-                    return false;
+                    Console.WriteLine("Production not matched. Backtracking...");
+                    currentPosition = originalPosition; // Reset the position and try the next production
                 }
             }
 
-            // Parsing is successful if both lists are empty.
-            if (tokenList.Count == 0 && productionList.Count == 0)
-            {
-                Console.WriteLine("Parsing successful!");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Parsing failed: Lists not empty.");
-                return false;
-            }
-        }
-
-        public void SetProductionList(List<string> production)
-        {
-            productionList = production;
+            Console.WriteLine($"Parsing {nonTerminal} failed.");
+            return false; // Parsing failed for this non-terminal
         }
     }
 }
